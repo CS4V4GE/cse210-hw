59edaf8ac389af48870ffc2c988a5e7176e8d47b
Chris <chris.savage18.k@gmail.com>
2025-10-02 16:31:51 -0700

    added resume project

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,27 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+
+        Job job1 = new Job();
+        job1._jobTitle = "Team Associate";
+        job1._company = "Walmart";
+        job1._startYear = 2023;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Sales Manager";
+        job2._company = "Pointe Marketing";
+        job2._startYear = 2025;
+        job2._endYear = 2025;
+
+        Resume myResume = new Resume();
+        myResume._name = "Christopher Savage";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
